eebbb0637f939ba4fe6534cbba1b85f68b3a6f5b
Aidar <aidarkhalilov4021@gmail.com>
2022-03-03 00:01:25 +0300

    Fixed javascript file, reference to greeting page

--- src/main/resources/static/js/reg.js
+++ src/main/resources/static/js/reg.js
@@ -9,3 +9,15 @@ function show_hide_password(target) {
     }
     return false;
 }
+
+function show_hide_passwords(target) {
+    var input = document.getElementById('passwords-input');
+    if (input.getAttribute('type') == 'password') {
+        target.classList.add('view');
+        input.setAttribute('type', 'text');
+    } else {
+        target.classList.remove('view');
+        input.setAttribute('type', 'password');
+    }
+    return false;
+}
